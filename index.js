8c26d2823483f849a3fcbfcc7c021b424090739f
TaeJiramet <674259004@webmail.npru.ac.th>
2026-08-27 16:37:13 +0700

    first commit

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 import express from "express";
 import { Product, connectDB } from "./db.js";
+import cors from "cors";
 
 const app = express();
 const PORT = 5000;
 
+app.use(cors());
 app.use(express.json());
 
 connectDB();
